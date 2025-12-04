845644e1fd07bdfa50bac432a6857467044459f3
Stephan <stephan@laetum.de>
2025-12-04 16:34:17 +0100

    Spring Boot project initialized with MySQL connection

---
+++ src/main/java/de/laetum/pmbackend/PmBackendApplication.java
@@ -0,0 +1,13 @@
+package de.laetum.pmbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PmBackendApplication.class, args);
+	}
+
+}
